3faf277e28c4a6e4a28667d1c9f5bb51d96e7ac8
armishiftikhar <armishiftikhar8@gmail.com>
2024-08-20 04:09:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+// Printing a Wellcome Message 
+console.log("\n\tWellcome To \'ArmishIftikhar\' - CLI Simple Calculator\n");
+// Asking Questions from users through Inquirer 
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Division", "Multiplication"],
+    },
+]);
+// Conditional statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invaild Input");
+}
